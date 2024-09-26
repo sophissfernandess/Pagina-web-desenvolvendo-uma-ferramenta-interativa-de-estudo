71e858e916976c88f373138c575aeb70b0a98c86
sophissfernandess <143853925+sophissfernandess@users.noreply.github.com>
2024-09-25 22:24:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Felicidades',
+    'O que precisa para ser feliz?',
+    ' amor e dinheiro'
+)
+
+criaCartao(
+    'ALUNOS',
+    'Qual é o insta da aluna mais linda?',
+    '@sophia_fernandess'
+)
+
+criaCartao(
+    'Qual é a melhor turma do CERB',
+    'A que mais se dedica as aulas do Prof. Rafael?',
+    'Turma 2° ano B'
+)
+
+criaCartao(
+    'Palmeiras',
+    'Quantos titulo mundial o palmeiras têm?',
+    'Nenhum 🤣🤣🤣'
+)
